fd9416f5eae386a8f08df8f75e516575cdb9107d
pkoton <pkoton@users.noreply.github.com>
2016-08-04 15:37:51 -0400

    updated linkitz.js
    moved color conversion functions to colour.js

--- deps/linkitz-blockly/src/generators/dart/linkitz.js
+++ deps/linkitz-blockly/src/generators/dart/linkitz.js
@@ -36,39 +36,6 @@ Blockly.Dart['flash_leds'] = function(block) {
     return code;
 }
 
-// convert a hexidecimal color string to 0..255 R,G,B, remember that first char is ' and second char is #
-// example input: '#ff00cc'
-
-function hexToRGB (hex){
-    var r = parseInt(hex.substr(2,2),16);
-    var g = parseInt(hex.substr(4,2),16);
-    var b = parseInt(hex.substr(6,2),16);
-    return [r,g,b];
-}
-
-// convert (h,s,v) to [r,g,b]
-
-function HSVtoRGB(h, s, v) {
-    var r, g, b, i, f, p, q, t;
-    var normH = (h % 360)/360; 
-    i = Math.floor(normH * 6);
-    f = normH * 6 - i;
-    p = v * (1 - s);
-    q = v * (1 - f * s);
-    t = v * (1 - (1 - f) * s);
-    switch (i % 6) {
-        case 0: r = v, g = t, b = p; break;
-        case 1: r = q, g = v, b = p; break;
-        case 2: r = p, g = v, b = t; break;
-        case 3: r = p, g = q, b = v; break;
-        case 4: r = t, g = p, b = v; break;
-        case 5: r = v, g = p, b = q; break;
-    }
-        r = Math.round(r * 255);
-        g = Math.round(g * 255);
-        b = Math.round(b * 255);
-    return [r,g,b];
-}
 
 // **************************************************************************************************
 // MOTION  MOTION  MOTION  MOTION  MOTION  MOTION  MOTION MOTION  MOTION  MOTION  MOTION
