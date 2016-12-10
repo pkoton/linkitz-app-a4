3504c6c8d5603c80fd26ab1327a7649828631545
Drew Macrae <Drew@Linkitz.com>
2016-12-10 10:04:00 -0800

    Fixed Windows Build that was broken in September
    1 click windows build needed fixing. I guess these things happen when I
    take a month or so to deal with other stuff.

--- install.bat
+++ install.bat
@@ -8,5 +8,8 @@ call python js\services\hexfirmware2js.py
 del build\js\services\firmware.js
 copy js\services\firmware.js build\js\services\firmware.js
 
+call npm install
 call grunt shell
+call grunt concat
 call grunt copy
+call grunt compress
