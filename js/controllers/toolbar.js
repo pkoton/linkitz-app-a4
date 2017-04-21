ed2caded1620bf504b7c58f17faab3ea73746048
LNeel <lyssak@gmail.com>
2017-04-21 11:01:59 -0400

    improved UX
    - made one bottom combining connect/disconnect and generate-upload code

--- js/controllers/toolbar.js
+++ js/controllers/toolbar.js
@@ -11,41 +11,48 @@ angular.module('linkitzApp').controller('ToolbarController', [
     'minimumFirmwareVersion',
     function($scope, $timeout, LogService, LinkitzToy, Messager, errorCatcher, ChromeBrowser, minimumFirmwareVersion) {
 
-	$scope.connectTransitioning = false;
-
-	$scope.toggleConnect = function toggleConnect()
-	{
-        $scope.savedDropdownOpen = false;
-		$scope.connectTransitioning = true;
-		if (!$scope.isConnected) {
-			LinkitzToy.connect()
-				.then(function () {
-                    return LinkitzToy.verifyDevice();
-                })
-                .then(function (querybytes) {
-                    $scope.setConnected(true);
-                    return LinkitzToy.readID();
-                })
-                .then(function (connectedID) {
-                    $scope.setHubID(connectedID);
-                })
-                .catch(function (reason) {
-                    $scope.connectTransitioning = false;
-                    errorCatcher.handle("Error connecting to Linkitz", reason);
-                });
-		}
-		else {
-			LinkitzToy.disconnect()
-				.then(function () {
-                    $scope.setConnected(false);
-					$scope.connectTransitioning = false;
-				})
-                .catch(function (reason) {
-                    $scope.connectTransitioning = false;
-                    errorCatcher.handle("Error disconnecting from Linkitz", reason);
-                });
-		}
-	}
+//	$scope.connectTransitioning = false;
+//
+//	$scope.toggleConnect = function toggleConnect()
+//	{
+//        $scope.connectTransitioning = true;
+//	var catch_msg = "Error connecting to Linkitz";
+//	LinkitzToy.connect()
+//	.then(function () {
+//            return LinkitzToy.verifyDevice();
+//        })
+//        .then(function (querybytes) {
+//            $scope.setConnected(true);
+//            return LinkitzToy.readID();
+//        })
+//        .then(function (connectedID) {
+//            $scope.setHubID(connectedID);
+//        })
+//	.then(function () {
+//            catch_msg = "Error programming Linkitz";
+//	})
+//        .then($scope.generateCode)
+//        .then(HexGenerator.processAssembly)
+//        .then(LinkitzToy.programDevice) // in linkitztoy.js line 252 (aliased linkitzProgramDevice)
+//        .then(function programSuccess() {
+//            LogService.appLogMsg("Programming Successful. Signing...");
+//        })
+//        .then(LinkitzToy.signFlash) 
+//        .then(function signSuccess() {
+//            LogService.appLogMsg("Signed.");
+//        })
+//	.then(function () {
+//            catch_msg = "Error disconnecting from Linkitz";
+//	})
+//        .then(LinkitzToy.disconnect)
+//	.then(function () {
+//            $scope.setConnected(false);
+//	})
+//        .catch(function (reason) {
+//            $scope.connectTransitioning = false;
+//            errorCatcher.handle(catch_msg, reason);
+//	});	
+//	}
 
     $scope.savedDropdownOpen = false;
     $scope.savedProgramList = null;
