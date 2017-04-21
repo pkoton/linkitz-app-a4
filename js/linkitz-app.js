246d7679002a8c3780f0ecf2239d1deab0cead40
LNeel <lyssak@gmail.com>
2017-04-21 11:03:48 -0400

    improved UX
    combined connect/disconnect and build/upload into one function

--- js/linkitz-app.js
+++ js/linkitz-app.js
@@ -132,22 +132,112 @@ linkitzApp.controller('LinkitzAppController', [
         return $scope.editor.deferredCode.promise;
     }
 
-    $scope.buildAndUploadProgram = function buildAndUploadProgram () {
-        $scope.generateCode()
-            .then(HexGenerator.processAssembly)
-            .then(LinkitzToy.programDevice)
-            .then(function programSuccess() {
-                LogService.appLogMsg("Programming Successful. Signing...");
-            })
-            .then(LinkitzToy.signFlash)
-            .then(function signSuccess() {
-                LogService.appLogMsg("Signed.");
+    $scope.connectTransitioning = false;
+
+	$scope.toggleConnect = function toggleConnect()
+	{
+	    $scope.connectTransitioning = true;
+	    var catch_msg = "Error connecting to Linkitz";
+	    LinkitzToy.connect()
+	    .then(function () {
+		return LinkitzToy.verifyDevice();
+	    })
+	    .then(function (querybytes) {
+		$scope.setConnected(true);
+		return LinkitzToy.readID();
+	    })
+	    .then(function (connectedID) {
+		$scope.setHubID(connectedID);
+	    })
+	    .then(function () {
+		catch_msg = "Error programming Linkitz";
+	    })
+	    .then($scope.generateCode)
+	    .then(HexGenerator.processAssembly)
+	    .then(LinkitzToy.programDevice) // in linkitztoy.js line 252 (aliased linkitzProgramDevice)
+	    .then(function programSuccess() {
+		LogService.appLogMsg("Programming Successful. Signing...");
+	    })
+	    .then(LinkitzToy.signFlash) 
+	    .then(function signSuccess() {
+		LogService.appLogMsg("Signed.");
+	    })
+	    .then(function () {
+		catch_msg = "Error disconnecting from Linkitz";
+	    })
+	    .then(LinkitzToy.disconnect)
+	    .then(function () {
+		$scope.setConnected(false);
+		$scope.connectTransitioning = false;
+	    })
+	    .catch(function (reason) {
+		$scope.connectTransitioning = false;
+		errorCatcher.handle(catch_msg, reason);
+	    });	
+	}
+
+    
+    function tryConnect() {
+	var deferred = $q.defer();
+	LinkitzToy.connect()
+		.then(function () {
+		    return LinkitzToy.verifyDevice();
+		   })
+	        .then(function (querybytes) {
+		       $scope.setConnected(true);
+		       return LinkitzToy.readID();
+		   })
+		.then(function (connectedID) {
+		    $scope.setHubID(connectedID);
+		   })
+		.then(function () {
+                deferred.resolve();                                             // programming complete
             })
             .catch(function (reason) {
-                errorCatcher.handle("Error programming Linkitz", reason);
+                deferred.reject(reason);
             });
-    }
 
+        return deferred.promise;
+    }
+    
+    function tryBuild() {
+	var deferred = $q.defer();
+	generateCode()
+        .then(HexGenerator.processAssembly)
+        .then(LinkitzToy.programDevice) // def in linkitztoy.js line 252 (aliased linkitzProgramDevice)
+        .then(function programSuccess() {
+            LogService.appLogMsg("Programming Successful. Signing...");
+        })
+        .then(LinkitzToy.signFlash) 
+        .then(function signSuccess() {
+            LogService.appLogMsg("Signed.");
+        })
+        .then(function () {
+                deferred.resolve();                                             // programming complete
+            })
+            .catch(function (reason) {
+                deferred.reject(reason);
+            });
+
+        return deferred.promise;
+    }
+    
+    function tryDisconnect() {
+	var deferred = $q.defer();
+	LinkitzToy.disconnect()
+	.then(function () {
+            $scope.setConnected(false);
+	})
+	.then(function () {
+                deferred.resolve();                                             // programming complete
+            })
+            .catch(function (reason) {
+                deferred.reject(reason);
+            });
+
+        return deferred.promise;
+    }
+    
     $scope.toggleDevMode = function toggleDevMode() {
         $scope.devMode = !$scope.devMode;
         LogService.setDevMode($scope.devMode);
