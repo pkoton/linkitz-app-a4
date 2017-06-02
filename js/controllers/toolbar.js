0d482f5f9558461d15b0171d360506924a92e079
LNeel <lyssak@gmail.com>
2017-06-02 13:59:11 -0400

    moved to linkitz-app.js

--- js/controllers/toolbar.js
+++ js/controllers/toolbar.js
@@ -11,37 +11,7 @@ angular.module('linkitzApp').controller('ToolbarController', [
     'minimumFirmwareVersion',
     function($scope, $timeout, LogService, LinkitzToy, Messager, errorCatcher, ChromeBrowser, minimumFirmwareVersion) {
 
-function linkitzPing(){
-	if (!$scope.connectTransitioning) { //don't ping if we are already connected
-		LinkitzToy.connect()
-			.then(function () {
-			    return LinkitzToy.verifyDevice();
-			   })
-			.then(function (querybytes) {
-			       $scope.setConnected(true);
-			       $scope.setAttached(true);
-			       return LinkitzToy.readID();
-			   })
-			.then(function (connectedID) {
-			    $scope.setHubID(connectedID);
-			    console.log("Pinging hub " + connectedID[0].toString(16) + ':' +
-				 connectedID[1].toString(16) + ':' +
-				 connectedID[2].toString(16) + ':' +
-				 connectedID[3].toString(16) );
-			   })
-			.then(LinkitzToy.disconnect)
-			.then(function () {
-			$scope.setConnected(false);
-		    })
-		    .catch(function (reason) {
-			console.log("Ping: No hub detected");
-			$scope.setAttached(false);
-		    });
-	}
-    }
-    
-    setInterval(function(){
-        linkitzPing()}, 5000)
+
 
     $scope.savedDropdownOpen = false;
     $scope.savedProgramList = null;
