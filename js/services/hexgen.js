d09cc4a1a054518c1513b9bdc3cb8cece39614c1
Drew Macrae <Drew@Linkitz.com>
2017-07-17 22:55:54 -0700

    Let's be a bit less verbose during logging

--- js/services/hexgen.js
+++ js/services/hexgen.js
@@ -414,22 +414,22 @@ function linkitzApp_hexgen_generate_hex(assembly_code) {
         if(tokens[0].match(/goto/i)){
             linkhex_line = linkitzApp_hexgen_pad_words("60");
             
-            console.log("matching label:"+tokens[1]);
+            //console.log("matching label:"+tokens[1]);
             
             var targetAddr = labels[tokens[1]];
             
-            console.log("targetAddr:"+targetAddr);
+            //console.log("targetAddr:"+targetAddr);
             linkhex_line+=linkitzApp_hexgen_pad_words(linkitzApp_hexgen_byte_2_hex(0x80+Math.floor((targetAddr/2)/256)));
             linkhex_line+=linkitzApp_hexgen_pad_words(linkitzApp_hexgen_byte_2_hex(((targetAddr/2)%256)));
             hex_output+=linkitzApp_hexgen_make_hex_line(linkedaddr,linkhex_line);
         } else if(tokens[0].match(/syscall/i)&&tokens[1].match(/fcall/i)){
             linkhex_line = linkitzApp_hexgen_pad_words("61");
             
-            console.log("matching label:"+tokens[1]);
+            //console.log("matching label:"+tokens[1]);
             
             var targetAddr = labels[tokens[2]];
             
-            console.log("targetAddr:"+targetAddr);
+            //console.log("targetAddr:"+targetAddr);
             
             linkhex_line+=linkitzApp_hexgen_pad_words(linkitzApp_hexgen_byte_2_hex(0x80+Math.floor((targetAddr/2)/256)));
             linkhex_line+=linkitzApp_hexgen_pad_words(linkitzApp_hexgen_byte_2_hex(((targetAddr/2)%256)));
@@ -438,7 +438,7 @@ function linkitzApp_hexgen_generate_hex(assembly_code) {
             hex_output+=linkitzApp_hexgen_make_hex_line(linkedaddr,linkhex_line);
         }
         
-        console.log("token list:"+tokens+" Places hex:"+linkhex_line+" At addr:"+linkedaddr);
+        //console.log("token list:"+tokens+" Places hex:"+linkhex_line+" At addr:"+linkedaddr);
         
     }
     hex_output+=":00000001FF\n";
