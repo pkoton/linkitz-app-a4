8a18e8725feb8bc78497d6c40ca7dd3bc72246a8
pkoton <pkoton@users.noreply.github.com>
2017-12-13 17:39:38 -0500

    trying to pass alerts up from generator

--- js/controllers/modals/alert.js
+++ js/controllers/modals/alert.js
@@ -1,5 +1,6 @@
 // alert.js
 
 angular.module('linkitzApp').controller('AlertController', ['$scope', 'errorObject', function($scope, errorObject) {
+	$scope.connectTransitioning = false;
 	$scope.errorObject = errorObject;
 }]);
